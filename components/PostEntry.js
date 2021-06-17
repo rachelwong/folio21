98e98102f7e43abb5dd2420fb0b2953e0f44f84d
rachel-wong <hello.rachelwong@gmail.com>
2021-06-17 19:40:45 +1000

    Established connection to contentful & basic component setup

---
+++ components/PostEntry.js
@@ -0,0 +1,13 @@
+// Component: A single post entry on the blog page which has a listing
+// Parent: Get data from /pages/blog.js
+// Child: n/a
+
+const PostEntry = ({ post }) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default PostEntry
---
+++ pages/blog.js
@@ -0,0 +1,36 @@
+import { createClient} from 'contentful';
+import PostEntry from '../components/PostEntry'
+
+// initialise connection with contentful backend
+export const getStaticProps = async () => {
+
+  const client = createClient({
+    space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_CONTENTFUL_CDN_API
+  })
+
+  const res = await client.getEntries({ content_type: 'post'})
+    return {
+      props: {
+        posts: res.items
+      },
+      revalidate: 1 // to regenerate and requery for data when there are updates
+    }
+}
+
+const blog = ({ posts }) => {
+  console.log("posts", posts)
+  if (posts.length == 0) { return "" }
+  return (
+    <div>
+      <h1>Blog</h1>
+      <div className="blog-container">
+        {posts.length > 0 && posts.map((post, postIndex) => {
+          <PostEntry post={post} key={ postIndex} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default blog
