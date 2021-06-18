3aad754bddaa62ffd23ccb81ed3eb230f5d8f573
rachel-wong <hello.rachelwong@gmail.com>
2021-06-18 21:28:28 +1000

    Added styling & structure to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,19 @@
+import Carousel from '../components/Carousel'
+import Link from 'next/link'
+import styles from '../styles/Notfound.module.scss'
 const NotFound = () => {
   return (
-    <div>
-      <h2>404 error page not found </h2>
+    <div className={styles.container}>
+      <Carousel />
+      <div className={styles['notfound-content']}>
+        <h3 className={ styles['notfound-content__title']}>Wrong way</h3>
+        <p className={styles['notfound-content__blurb']}>The page you're looking for doesn't exist. Please head back home or send me an <a className={ styles['notfound-content__blurb__link']} href="mailto:hello.rachelwong@gmail.com">email</a></p>
+        <Link href="/">
+          <a className={styles['notfound-content__btn'] }>
+            Return home
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
