7ef0f096f82c3443681d13ce8a93addc74fbb571
rachel-wong <hello.rachelwong@gmail.com>
2021-06-19 10:37:44 +1000

    Add page transition

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 > To run: `npm run dev`
 
-> Deployed: https://folio21.vercel.app/
-
-
 ### Helpful links
 
-* rendering code snippets from contentful https://www.christiancoda.com/blog/code-snippets-with-contentful/
+* rendering code snippets from contentful
+https://www.christiancoda.com/blog/code-snippets-with-contentful/
+
+https://stackoverflow.com/questions/64191373/how-to-add-code-block-for-code-snippets-for-in-the-data-displays-in-the-react-ho
+
 * wrapping code snippets with <pre></pre> https://stackoverflow.com/questions/57149824/how-to-format-code-snippets-with-pre-tags-using-contentfuls-rich-text-react-r
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
 import '../styles/globals.scss'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { motion } from 'framer-motion'
 
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <>
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" transition={{duration: 0.5}} variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      }
+    }}>
       <Navbar />
         <Component {...pageProps} />
       <Footer />
-    </>
+    </motion.div>
   )
 }
 
