ba8ac7be5ef95379333360fc397bc6be066c2b58
rachel-wong <hello.rachelwong@gmail.com>
2021-06-24 19:49:27 +1000

    Disabled thumbnail image from blog entries

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ https://stackoverflow.com/questions/64191373/how-to-add-code-block-for-code-snip
 * wrapping code snippets with <pre></pre> https://stackoverflow.com/questions/57149824/how-to-format-code-snippets-with-pre-tags-using-contentfuls-rich-text-react-r
 
 * framer motion text reveal https://brad-carter.medium.com/how-to-animate-a-text-reveal-effect-in-react-with-framer-motion-ae8ddd296f0d (interestingly emojis doesn't work with this)
+
+* https://github.com/vercel/next.js/discussions/12810 extra module import in `next.config.js` to import sound into nextJS
--- components/PostEntry.js
+++ components/PostEntry.js
@@ -13,11 +13,11 @@ const PostEntry = ({ post }) => {
   return (
     <Link href={'/blog/' + slug} >
       <a className={styles['post-entry-wrapper']}>
-        <div className={ styles['post-entry-imagewrap']}>
+        {/* <div className={ styles['post-entry-imagewrap']}>
           <Image src={'https:' + featureImage.fields.file.url} width={featureImage.fields.file.details.image.width}
           height={featureImage.fields.file.details.image.height}
               alt={title} />
-        </div>
+        </div> */}
         <time className={styles['post-entry-date']} dateTime={ publishedDate }>{parsedDate[2] + " " + month + " "+ parsedDate[0]}</time>
         <h3 className={ styles['post-entry-title']}>{ title }</h3>
         <ul className={ styles['post-entry-tags']}>{tags.length > 0 && tags.map((tag, tagIndex) => (
